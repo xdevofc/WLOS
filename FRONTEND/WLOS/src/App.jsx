33fac1c7c9793c60c8332476c52f5e637cca9f6a
xdev <xdevofc@gmail.com>
2025-06-09 21:25:06 -0300

    setting up project

---
+++ FRONTEND/WLOS/src/App.jsx
@@ -0,0 +1,13 @@
+import MainMenu from "./Pages/Menus/MainMenu"
+
+const App = () => {
+
+  return (
+    <>
+    <h1>Hello world from App</h1>
+    <MainMenu/>
+    </>
+  )
+}
+
+export default App
---
+++ FRONTEND/WLOS/src/Pages/Menus/MainMenu.jsx
@@ -0,0 +1,13 @@
+
+const MainMenu = () => {
+
+    return (
+        <>
+            <h1>Hello world from MainMenu</h1>
+            <button>Crear</button>
+            <button>Importar</button>
+        </>
+    )
+}
+
+export default MainMenu
